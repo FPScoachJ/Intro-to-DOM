9574608741873ef379637c8f42687c096612c306
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-05-01 08:55:54 -0700

    More Dom Manipulation
    Grab, Make, Edit, Append

--- dom.js
+++ dom.js
@@ -1,12 +1,22 @@
-console.log(document);
+// console.log(document);
 
-console.log(document.getElementsByClassName("howdyheader"));
-console.log(document.getElementById("firstHeader").innerText);
+// console.log(document.getElementsByClassName("howdyheader"));
+// console.log(document.getElementById("firstHeader").innerText);
 
-const byID = document.querySelector("#lorem").innerText
-const byClass = document.querySelector("#lorem").innerText
-console.log(byID);
-console.log(byClass);
+// const byID = document.querySelector("#lorem").innerText
+// const byClass = document.querySelector("#lorem").innerText
+// console.log(byID);
+// console.log(byClass);
 
+//grab the container first
+const studentContainer = document.querySelector(".students");
+// make your element
+const studentDiv = document.createElement("div");
+const dustin = document.createElement("p");
+const kim = document.createElement("p");
+dustin.innerText = "Dustin";
+kim.innerText = "Kim";
 
-document.get
+//put the element in the container
+studentDiv.append(dustin, kim);
+studentContainer.append(studentDiv);
