84a57ced1b6fb4966ec08a8f33f703a1429628fb
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-05-01 08:05:40 -0700

    Created Initial Files
    DOM Elements

---
+++ dom.js
@@ -0,0 +1,12 @@
+console.log(document);
+
+console.log(document.getElementsByClassName("howdyheader"));
+console.log(document.getElementById("firstHeader").innerText);
+
+const byID = document.querySelector("#lorem").innerText
+const byClass = document.querySelector("#lorem").innerText
+console.log(byID);
+console.log(byClass);
+
+
+document.get
